2b06335de4325d565652de04bb621ea52dbbc29d
zyflool <532256565@qq.com>
2019-03-06 00:32:30 +0800

    FinshLogin

--- app/src/main/java/com/example/homeworks/WorkActivity.java
+++ app/src/main/java/com/example/homeworks/WorkActivity.java
@@ -1,5 +1,15 @@
 package com.example.homeworks;
 
-class WorkActivity {
-    startActity(intent)
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WorkActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignmentlayout);
+
+    }
 }
