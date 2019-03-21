467fc6e9dc9116642db78f62ee99eb8315ffc37d
Ljl233 <`2955441566@qq.com`>
2019-03-22 00:10:44 +0800

    DetailActivity2

--- app/src/main/java/com/example/homeworks/AssignDetail/DetailActivity.java
+++ app/src/main/java/com/example/homeworks/AssignDetail/DetailActivity.java
@@ -1,12 +1,30 @@
 package com.example.homeworks.AssignDetail;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.homeworks.AssignActivity;
+import com.example.homeworks.CourseAndTaskActivity;
 import com.example.homeworks.R;
+import com.example.homeworks.RetrofitWrapper;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView tv_begin2, tv_ddl2, tv_state, tv_content, tv_pointNum, tv_studentNum, tv_submitContent,
+            tv_feedback, tv_assignAttachmentNum, tv_assignAttachment_name, tv_assignName2;
+
+    String siteId, assignId, cookie, token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +33,83 @@ public class DetailActivity extends AppCompatActivity {
         initView();
     }
 
-    private void initView() {
-        TextView tv_begin2, tv_ddl2, tv_state, tv_content, tv_pointNum, tv_studentNum, tv_submitContent,
-                tv_feedback, tv_assignAttachmentNum, tv_assignAttachment_name;
+
+    void initView() {
+        Toolbar toolbar_detail = findViewById(R.id.toolbar_detail);
+
+        tv_assignAttachment_name = findViewById(R.id.tv_assignAttachment_name);
+        tv_assignAttachmentNum = findViewById(R.id.tv_assignAttachmentNum);
+        tv_begin2 = findViewById(R.id.tv_begin2);
+        tv_ddl2 = findViewById(R.id.tv_ddl2);
+        tv_state = findViewById(R.id.tv_state);
+        tv_content = findViewById(R.id.tv_content);
+        tv_pointNum = findViewById(R.id.tv_pointNum);
+        tv_submitContent = findViewById(R.id.tv_submitContent);
+        tv_feedback = findViewById(R.id.tv_feedback);
+        tv_studentNum = findViewById(R.id.tv_studentNum);
+        tv_assignName2 = findViewById(R.id.tv_assignName2);
+
+        setSupportActionBar(toolbar_detail);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        request();
+
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(DetailActivity.this, AssignActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void request() {
+        DetailService detailService = RetrofitWrapper.getInstance().create(DetailService.class);
+        Call<DetailBean> call = detailService.getDetailBean(siteId, assignId, cookie, token);
+        call.enqueue(new Callback<DetailBean>() {
+            @Override
+            public void onResponse(Call<DetailBean> call, Response<DetailBean> response) {
+                if (response.isSuccessful()) {
+                    DetailBean detail = response.body();
+                    initDetail(detail);
+
+                } else {
+                    Toast.makeText(DetailActivity.this, "请求失败，请重试", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DetailBean> call, Throwable t) {
+                Toast.makeText(DetailActivity.this, "404", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void initDetail(DetailBean detail) {
+        tv_begin2.append(detail.getBeginTime());
+        tv_ddl2.append(detail.getEndTime());
+        tv_assignName2.append(detail.getCourseName());
+        tv_assignName2.append(":");
+        tv_assignName2.append(detail.getAssignName());
+
+
+        if (detail.getStatus() == "0") tv_state.append("未提交");
+        else if (detail.getStatus() == "1") tv_state.append("待批阅");
+        else if (detail.getStatus() == "2") tv_state.append("已驳回");
+        else tv_state.append("已批阅");
+
+        tv_content.append(detail.getContent());
+        tv_pointNum.append(detail.getPointNum());
+        tv_studentNum.append(detail.getStudentNum());
+        tv_submitContent.append(detail.getSubmitContent());
+        tv_feedback.append(detail.getFeedback());
+
+
+    }
 }
--- app/src/main/java/com/example/homeworks/AssignDetail/DetailBean.java
+++ app/src/main/java/com/example/homeworks/AssignDetail/DetailBean.java
@@ -1,5 +1,342 @@
 package com.example.homeworks.AssignDetail;
 
+import java.util.List;
+
 public class DetailBean {
-   
+
+    /**
+     * msg : String
+     * cookie : String
+     * siteId : String
+     * assignId : String
+     * courseName : String
+     * assignName : String
+     * status : Int
+     * beginTime : Int
+     * endTime : Int
+     * content : String
+     * pointNum : Int
+     * commitNum : Int
+     * isGroup : Int
+     * groupNum : Int
+     * studentNum : Int
+     * groupPoint : Int
+     * personalPoint : Int
+     * feedback : String
+     * assignAttachmentNum : Int
+     * assignAttachment : [{"id":"String","name":"String","ext":"String","sourceUrl":"string"}]
+     * submitAttachmentNum : Int
+     * submitAttachment : [{"id":"String","name":"String","ext":"String","uploadTime":"Int","sourceUrl":"String"}]
+     * submitContent : String
+     */
+
+    private String msg;
+    private String cookie;
+    private String siteId;
+    private String assignId;
+    private String courseName;
+    private String assignName;
+    private String status;
+    private String beginTime;
+    private String endTime;
+    private String content;
+    private String pointNum;
+    private String commitNum;
+    private String isGroup;
+    private String groupNum;
+    private String studentNum;
+    private String groupPoint;
+    private String personalPoint;
+    private String feedback;
+    private String assignAttachmentNum;
+    private String submitAttachmentNum;
+    private String submitContent;
+    private List<AssignAttachmentBean> assignAttachment;
+    private List<SubmitAttachmentBean> submitAttachment;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public String getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(String siteId) {
+        this.siteId = siteId;
+    }
+
+    public String getAssignId() {
+        return assignId;
+    }
+
+    public void setAssignId(String assignId) {
+        this.assignId = assignId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getAssignName() {
+        return assignName;
+    }
+
+    public void setAssignName(String assignName) {
+        this.assignName = assignName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPointNum() {
+        return pointNum;
+    }
+
+    public void setPointNum(String pointNum) {
+        this.pointNum = pointNum;
+    }
+
+    public String getCommitNum() {
+        return commitNum;
+    }
+
+    public void setCommitNum(String commitNum) {
+        this.commitNum = commitNum;
+    }
+
+    public String getIsGroup() {
+        return isGroup;
+    }
+
+    public void setIsGroup(String isGroup) {
+        this.isGroup = isGroup;
+    }
+
+    public String getGroupNum() {
+        return groupNum;
+    }
+
+    public void setGroupNum(String groupNum) {
+        this.groupNum = groupNum;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getGroupPoint() {
+        return groupPoint;
+    }
+
+    public void setGroupPoint(String groupPoint) {
+        this.groupPoint = groupPoint;
+    }
+
+    public String getPersonalPoint() {
+        return personalPoint;
+    }
+
+    public void setPersonalPoint(String personalPoint) {
+        this.personalPoint = personalPoint;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getAssignAttachmentNum() {
+        return assignAttachmentNum;
+    }
+
+    public void setAssignAttachmentNum(String assignAttachmentNum) {
+        this.assignAttachmentNum = assignAttachmentNum;
+    }
+
+    public String getSubmitAttachmentNum() {
+        return submitAttachmentNum;
+    }
+
+    public void setSubmitAttachmentNum(String submitAttachmentNum) {
+        this.submitAttachmentNum = submitAttachmentNum;
+    }
+
+    public String getSubmitContent() {
+        return submitContent;
+    }
+
+    public void setSubmitContent(String submitContent) {
+        this.submitContent = submitContent;
+    }
+
+    public List<AssignAttachmentBean> getAssignAttachment() {
+        return assignAttachment;
+    }
+
+    public void setAssignAttachment(List<AssignAttachmentBean> assignAttachment) {
+        this.assignAttachment = assignAttachment;
+    }
+
+    public List<SubmitAttachmentBean> getSubmitAttachment() {
+        return submitAttachment;
+    }
+
+    public void setSubmitAttachment(List<SubmitAttachmentBean> submitAttachment) {
+        this.submitAttachment = submitAttachment;
+    }
+
+    public static class AssignAttachmentBean {
+        /**
+         * id : String
+         * name : String
+         * ext : String
+         * sourceUrl : string
+         */
+
+        private String id;
+        private String name;
+        private String ext;
+        private String sourceUrl;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getExt() {
+            return ext;
+        }
+
+        public void setExt(String ext) {
+            this.ext = ext;
+        }
+
+        public String getSourceUrl() {
+            return sourceUrl;
+        }
+
+        public void setSourceUrl(String sourceUrl) {
+            this.sourceUrl = sourceUrl;
+        }
+    }
+
+    public static class SubmitAttachmentBean {
+        /**
+         * id : String
+         * name : String
+         * ext : String
+         * uploadTime : Int
+         * sourceUrl : String
+         */
+
+        private String id;
+        private String name;
+        private String ext;
+        private String uploadTime;
+        private String sourceUrl;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getExt() {
+            return ext;
+        }
+
+        public void setExt(String ext) {
+            this.ext = ext;
+        }
+
+        public String getUploadTime() {
+            return uploadTime;
+        }
+
+        public void setUploadTime(String uploadTime) {
+            this.uploadTime = uploadTime;
+        }
+
+        public String getSourceUrl() {
+            return sourceUrl;
+        }
+
+        public void setSourceUrl(String sourceUrl) {
+            this.sourceUrl = sourceUrl;
+        }
+    }
 }
